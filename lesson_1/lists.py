22e8b9c9356316c445b0e43dfdf36c01a7f201ea
Baxtiyor96 <baxtiyorcoder3771@gmail.com>
2023-01-23 22:56:43 +0500

    add new lesson

---
+++ lesson_1/lists.py
@@ -0,0 +1,51 @@
+# l = [1,'number',1, [1,2,1,3]]
+# print(l.count(1))
+
+# x,y = 1,2
+# print(x + y)
+
+# arr = [[1,2,3],[4,5,6],[7,8,9]]
+# print(arr[0] + arr[1] + arr[2])
+
+# user = list('qovun')
+# ele_user = []
+# ele = user[1:4]
+# ele1 = user[0]
+# ele2 = user[-1]
+# print(user)
+# ele.append(ele1)
+# ele.insert(0, ele2)
+# str_ele = ''
+# for i in ele:
+#    str_ele += i
+# print(str_ele)
+
+# user = 'absadkfakfa'
+# print([i for i in user if i == 'a'])
+# def main(num):
+#    return num + 5
+# arr = [1,2,1,3]
+
+# print(map(main, arr))
+# print(list(map(main, arr)))
+
+
+# def odd(n):
+#    if n % 2 == 1:
+#       return n
+# arr = list(range(1,11))
+
+# print(list(filter(odd, arr)))
+
+
+arr1 = [1,2,3,6,4]
+arr2 = [7,1,5,9,3]
+# arr3 = [j for i in arr1 for j in arr2 if i == j]
+# print(arr3)
+arr3 = []
+for i in arr1:
+   for k in arr2:
+      if i != k:
+         arr3.append()
+print(arr3)
+
---
+++ lesson_1/str_methods.py
@@ -0,0 +1,30 @@
+# s = "12.0"
+# print(s.isdigit())
+# print('11ssA'.isalnum())
+# print('abs'.isalpha())
+# print('Aa'.isupper())
+# print('aa'.islower())
+# print('aa'.istitle())
+
+
+# password = input()
+
+# upper_letter = False
+# digit = False
+# symbol = False
+
+# if len(password) >= 6:
+#    for
+
+
+
+
+
+
+
+
+
+
+
+
+
